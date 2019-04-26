bad411bfd88cadc7f2c74703e9e8beb7787f8c12
Softwarica College Lab <softwarica@softwarica.com>
2019-04-26 08:30:54 +0545

    cameraopen

--- app/src/main/java/com/e/merocameraapp/MainActivity.java
+++ app/src/main/java/com/e/merocameraapp/MainActivity.java
@@ -1,13 +1,67 @@
 package com.e.merocameraapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnImage;
+    private ImageView image_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermission();
+        btnImage=findViewById(R.id.button_image);
+        image_view=findViewById(R.id.imageview);
+
+
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            loadCamera();
+            }
+        });
+
+    }
+    private void  checkPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]
+                    {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},0);
+        }
+    }
+    private void loadCamera(){
+        Intent intent =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(intent,1);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==1 && resultCode==RESULT_OK){
+            Bundle extras=data.getExtras();
+            Bitmap imageBitmap=(Bitmap) extras.get("data");
+            image_view.setImageBitmap(imageBitmap);
+        }
+    }
+    private String currentDateFormat(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HH_mm_ss");
+        String  currentTimeStamp = dateFormat.format(new Date());
+        return currentTimeStamp;
     }
 }
